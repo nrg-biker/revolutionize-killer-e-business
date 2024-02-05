7927dcabf80d431626550a9898200a09a7791bc5
nrg-biker <156887367+nrg-biker@users.noreply.github.com>
2024-02-05 03:09:40 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,25 @@
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+        for (int item : data) {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        }
+
+        int[] data = new int[10];
+
+        }
+    }
+            System.out.println("Random Number: " + item);
+    }
+        int[] data = new int[10];
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+
+        }
+        for (int item : data) {
+}
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
